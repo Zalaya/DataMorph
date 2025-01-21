127d6cca2b2524129fb8944678f7cf31e1340594
Zalaya <enol.corzal@gmail.com>
2025-01-21 19:26:50 +0100

    feat: add tests for the column type parser, there are some tests left, they will be made in the next commits

---
+++ src/test/java/org/zalaya/dataset/utilities/ColumnTypeParserTest.java
@@ -0,0 +1,54 @@
+package org.zalaya.dataset.utilities;
+
+import org.junit.jupiter.api.Test;
+import org.zalaya.dataset.enumerators.ColumnType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ColumnTypeParserTest {
+
+    @Test
+    public void shouldParseString() {
+        String value = "Hello";
+        Object expectedValue = "Hello";
+
+        assertEquals(expectedValue, ColumnTypeParser.parse(value, ColumnType.STRING));
+    }
+
+    @Test
+    public void shouldParseNumber() {
+        String value = "123";
+        Object expectedValue = 123;
+
+        assertEquals(expectedValue, ColumnTypeParser.parse(value, ColumnType.NUMBER));
+    }
+
+    @Test
+    public void shouldParseIntegerNumber() {
+        String value = "123";
+        Object expectedValue = 123;
+
+        assertEquals(expectedValue, ColumnTypeParser.parse(value, ColumnType.NUMBER));
+    }
+
+    @Test
+    public void shouldParseDoubleNumber() {
+        String value = "123.45";
+        Object expectedValue = 123.45;
+
+        assertEquals(expectedValue, ColumnTypeParser.parse(value, ColumnType.NUMBER));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenParsingInvalidNumber() {
+        String value = "123.45.67";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ColumnTypeParser.parse(value, ColumnType.NUMBER);
+        });
+    }
+
+    // TODO: Add tests for parsing dates
+
+}

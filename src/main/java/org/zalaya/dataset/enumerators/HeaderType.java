2a664ed89f5e0a2a47a743f510414ccce48a67db
Zalaya <enol.corzal@gmail.com>
2025-01-22 19:29:06 +0100

    feat: add some extra functionality for the header type enumerator and tests

--- src/main/java/org/zalaya/dataset/enumerators/HeaderType.java
+++ src/main/java/org/zalaya/dataset/enumerators/HeaderType.java
@@ -1,9 +1,25 @@
 package org.zalaya.dataset.enumerators;
 
+import java.util.Date;
+
 public enum HeaderType {
 
-    NUMBER,
-    STRING,
-    DATE
+    NUMBER(Number.class),
+    STRING(String.class),
+    DATE(Date.class);
+
+    private final Class<?> clazz;
+
+    HeaderType(Class<?> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("HeaderType class cannot be null");
+        }
+
+        this.clazz = clazz;
+    }
+
+    public Class<?> getClazz() {
+        return clazz;
+    }
 
 }

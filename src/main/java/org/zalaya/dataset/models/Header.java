d09fdd739f54910f9cc5b729a284cc59416c100b
Zalaya <enol.corzal@gmail.com>
2025-01-23 19:03:12 +0100

    feat: add a new exception for the creation of an invalid header

--- src/main/java/org/zalaya/dataset/models/Header.java
+++ src/main/java/org/zalaya/dataset/models/Header.java
@@ -3,13 +3,14 @@ package org.zalaya.dataset.models;
 import lombok.Builder;
 
 import org.zalaya.dataset.enumerators.HeaderType;
+import org.zalaya.dataset.exceptions.InvalidHeaderException;
 
 @Builder
 public record Header(String name, HeaderType type) {
 
     public Header {
         if (name == null || name.isBlank() || type == null) {
-            throw new IllegalArgumentException("Header name and type must not be null or empty");
+            throw new InvalidHeaderException("Header name and type must not be null or empty");
         }
     }
 

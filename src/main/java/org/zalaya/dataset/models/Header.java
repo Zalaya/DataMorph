1f8f5cd419c05c394eae7326ba67e556d1feb6fc
Zalaya <enol.corzal@gmail.com>
2025-01-23 23:27:48 +0100

    feat: add some new functionalities and refactor the header class

--- src/main/java/org/zalaya/dataset/models/Header.java
+++ src/main/java/org/zalaya/dataset/models/Header.java
@@ -1,18 +1,27 @@
 package org.zalaya.dataset.models;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import org.zalaya.dataset.enumerators.HeaderType;
 import org.zalaya.dataset.exceptions.InvalidHeaderException;
 
-public record Header(String name, HeaderType type) {
+@Getter
+@Builder
+@EqualsAndHashCode(exclude = {"type"})
+public class Header {
 
-    public Header {
+    private final String name;
+    private final HeaderType type;
+
+    public Header(String name, HeaderType type) {
         if (name == null || name.isBlank() || type == null) {
             throw new InvalidHeaderException("Header name and type must not be null or empty");
         }
-    }
 
-    public static HeaderBuilder builder() {
-        return new HeaderBuilder();
+        this.name = name;
+        this.type = type;
     }
 
 }
--- src/test/java/org/zalaya/dataset/models/HeaderTest.java
+++ src/test/java/org/zalaya/dataset/models/HeaderTest.java
@@ -1,7 +1,8 @@
 package org.zalaya.dataset.models;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.zalaya.dataset.enumerators.HeaderType;
 import org.zalaya.dataset.exceptions.InvalidHeaderException;
 
@@ -9,9 +10,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HeaderTest {
 
-    @Test
-    public void shouldThrowExceptionWhenNameIsNull() {
-        String name = null;
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldThrowExceptionWhenNameIsInvalid(String name) {
         HeaderType type = HeaderType.STRING;
 
         assertThrows(InvalidHeaderException.class, () -> {
@@ -19,23 +20,16 @@ public class HeaderTest {
         });
     }
 
-    @Test
-    public void shouldThrowExceptionWhenNameIsEmpty() {
-        String name = "";
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldThrowExceptionWhenNameIsInvalidForBuilder(String name) {
         HeaderType type = HeaderType.STRING;
 
         assertThrows(InvalidHeaderException.class, () -> {
-            new Header(name, type);
-        });
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenNameIsBlank() {
-        String name = " ";
-        HeaderType type = HeaderType.STRING;
-
-        assertThrows(InvalidHeaderException.class, () -> {
-            new Header(name, type);
+            Header.builder()
+                .name(name)
+                .type(type)
+                .build();
         });
     }
 
@@ -50,14 +44,27 @@ public class HeaderTest {
     }
 
     @Test
+    public void shouldThrowExceptionWhenTypeIsNullForBuilder() {
+        String name = "name";
+        HeaderType type = null;
+
+        assertThrows(InvalidHeaderException.class, () -> {
+            Header.builder()
+                .name(name)
+                .type(type)
+                .build();
+        });
+    }
+
+    @Test
     public void shouldInstantiateHeaderWhenNameAndTypeAreValid() {
         String name = "name";
         HeaderType type = HeaderType.STRING;
 
         Header header = new Header(name, type);
 
-        assertEquals(name, header.name());
-        assertEquals(type, header.type());
+        assertEquals(name, header.getName());
+        assertEquals(type, header.getType());
     }
 
     @Test
@@ -70,8 +77,43 @@ public class HeaderTest {
             .type(type)
             .build();
 
-        assertEquals(name, header.name());
-        assertEquals(type, header.type());
+        assertEquals(name, header.getName());
+        assertEquals(type, header.getType());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenComparingHeadersWithSameNameAndType() {
+        String name = "name";
+        HeaderType type = HeaderType.STRING;
+
+        Header header1 = new Header(name, type);
+        Header header2 = new Header(name, type);
+
+        assertEquals(header1, header2);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenComparingHeadersWithSameNameAndDifferentType() {
+        String name = "name";
+        HeaderType type1 = HeaderType.STRING;
+        HeaderType type2 = HeaderType.NUMBER;
+
+        Header header1 = new Header(name, type1);
+        Header header2 = new Header(name, type2);
+
+        assertEquals(header1, header2);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenComparingHeadersWithDifferentName() {
+        String name1 = "name1";
+        String name2 = "name2";
+        HeaderType type = HeaderType.STRING;
+
+        Header header1 = new Header(name1, type);
+        Header header2 = new Header(name2, type);
+
+        assertNotEquals(header1, header2);
     }
 
 }

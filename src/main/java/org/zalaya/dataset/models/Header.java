661e1069c02302a05280179599a991228df5ffbf
Zalaya <enol.corzal@gmail.com>
2025-01-23 18:54:41 +0100

    refactor: add lombok and refactor the header class

--- src/main/java/org/zalaya/dataset/models/Header.java
+++ src/main/java/org/zalaya/dataset/models/Header.java
@@ -1,29 +1,18 @@
 package org.zalaya.dataset.models;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+
 import org.zalaya.dataset.enumerators.HeaderType;
 
-import java.util.Objects;
-
+@Builder
+@EqualsAndHashCode(exclude = {"type"})
 public record Header(String name, HeaderType type) {
 
     public Header {
         if (name == null || name.isBlank() || type == null) {
             throw new IllegalArgumentException("Header name and type must not be null or empty");
         }
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Header header)) {
-            return false;
-        }
-
-        return Objects.equals(name, header.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(name);
     }
 
 }

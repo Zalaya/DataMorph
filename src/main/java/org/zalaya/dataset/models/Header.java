204fdf9ecae1b9cff7e44e690dd8571044b33af9
Zalaya <enol.corzal@gmail.com>
2025-01-23 18:57:24 +0100

    refactor: remove equals and hashcode annotation as records already provide those methonds

--- src/main/java/org/zalaya/dataset/models/Header.java
+++ src/main/java/org/zalaya/dataset/models/Header.java
@@ -1,12 +1,10 @@
 package org.zalaya.dataset.models;
 
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 
 import org.zalaya.dataset.enumerators.HeaderType;
 
 @Builder
-@EqualsAndHashCode(exclude = {"type"})
 public record Header(String name, HeaderType type) {
 
     public Header {

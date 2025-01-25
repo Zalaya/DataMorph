d1063569b2950a41cef46c54b1448dbe05120355
Zalaya <enol.corzal@gmail.com>
2025-01-25 21:13:18 +0100

    test: add the basic tests for the header type

--- src/test/java/org/zalaya/dataset/enumerators/HeaderTypeTest.java
+++ src/test/java/org/zalaya/dataset/enumerators/HeaderTypeTest.java
@@ -1,5 +1,47 @@
 package org.zalaya.dataset.enumerators;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class HeaderTypeTest {
+
+    @Test
+    void shouldReturnNumberClassWhenInvokingNumberType() {
+        // Arrange
+        HeaderType headerType = HeaderType.NUMBER;
+
+        // Act
+        Class<?> type = headerType.getType();
+
+        // Assert
+        assertEquals(Number.class, type);
+    }
+
+    @Test
+    void shouldReturnStringClassWhenInvokingStringType() {
+        // Arrange
+        HeaderType headerType = HeaderType.STRING;
+
+        // Act
+        Class<?> type = headerType.getType();
+
+        // Assert
+        assertEquals(String.class, type);
+    }
+
+    @Test
+    void shouldReturnLocalDateClassWhenInvokingDateType() {
+        // Arrange
+        HeaderType headerType = HeaderType.DATE;
+
+        // Act
+        Class<?> type = headerType.getType();
+
+        // Assert
+        assertEquals(LocalDate.class, type);
+    }
 
 }

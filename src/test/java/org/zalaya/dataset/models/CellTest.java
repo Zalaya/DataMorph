3d209b1d13acb6c131d6d2bea481df1de23dd1be
Zalaya <enol.corzal@gmail.com>
2025-01-25 21:10:14 +0100

    test: remove tests as we will remake them following a new workflow

--- src/test/java/org/zalaya/dataset/models/CellTest.java
+++ src/test/java/org/zalaya/dataset/models/CellTest.java
@@ -1,25 +1,5 @@
 package org.zalaya.dataset.models;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class CellTest {
-
-    @Test
-    void shouldReturnTrueWhenComparingCellsWithSameValues() {
-        Cell cell1 = new Cell(1);
-        Cell cell2 = new Cell(1);
-
-        assertEquals(cell1, cell2);
-    }
-
-    @Test
-    void shouldReturnFalseWhenComparingCellsWithDifferentValues() {
-        Cell cell1 = new Cell(1);
-        Cell cell2 = new Cell(2);
-
-        assertNotEquals(cell1, cell2);
-    }
 
 }

20312a82b8ff7f2d985709d55e7371e385204996
Zalaya <enol.corzal@gmail.com>
2025-01-25 18:31:55 +0100

    test: add parameterized tests for dataset name validation and enhance cell tests

--- src/test/java/org/zalaya/dataset/models/CellTest.java
+++ src/test/java/org/zalaya/dataset/models/CellTest.java
@@ -1,20 +1,10 @@
 package org.zalaya.dataset.models;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = { " ", "  ", "\t", "\n" })
-    void shouldNotThrowExceptionWhenValueIsNullOrBlank(String value) {
-        assertDoesNotThrow(() -> new Cell(value));
-    }
 
     @Test
     void shouldReturnTrueWhenComparingCellsWithSameValues() {
--- src/test/java/org/zalaya/dataset/models/DatasetTest.java
+++ src/test/java/org/zalaya/dataset/models/DatasetTest.java
@@ -1,5 +1,56 @@
 package org.zalaya.dataset.models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.zalaya.dataset.exceptions.specific.InvalidDatasetException;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class DatasetTest {
+
+    private Set<Header> headers;
+    private List<Row> rows;
+
+    @BeforeEach
+    void setUp() {
+        headers = Set.of();
+        rows = List.of();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { " " })
+    void shouldThrowExceptionWhenNameIsNullOrBlank(String name) {
+        assertThrows(InvalidDatasetException.class, () -> {
+            new Dataset(name, headers, rows);
+        });
+    }
+
+    @Test
+    void shouldReturnTrueWhenComparingDatasetsWithSameValues() {
+        String name = "name";
+
+        Dataset dataset1 = new Dataset(name, headers, rows);
+        Dataset dataset2 = new Dataset(name, headers, rows);
+
+        assertEquals(dataset1, dataset2);
+    }
+
+    @Test
+    void shouldReturnFalseWhenComparingDatasetsWithDifferentValues() {
+        String name1 = "name1";
+        String name2 = "name2";
+
+        Dataset dataset1 = new Dataset(name1, headers, rows);
+        Dataset dataset2 = new Dataset(name2, headers, rows);
+
+        assertNotEquals(dataset1, dataset2);
+    }
 
 }

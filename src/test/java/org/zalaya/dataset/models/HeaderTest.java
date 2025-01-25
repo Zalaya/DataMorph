d36d33459006674ce1e7c8e5a1b7cc95eddc083d
Zalaya <enol.corzal@gmail.com>
2025-01-25 20:05:32 +0100

    test: refactor header tests to use setup method and improve parameterized tests

--- src/test/java/org/zalaya/dataset/models/HeaderTest.java
+++ src/test/java/org/zalaya/dataset/models/HeaderTest.java
@@ -1,8 +1,10 @@
 package org.zalaya.dataset.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.zalaya.dataset.enumerators.HeaderType;
 import org.zalaya.dataset.exceptions.InvalidHeaderException;
@@ -11,36 +13,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeaderTest {
 
+    private HeaderType type;
+
+    @BeforeEach
+    void setUp() {
+        type = HeaderType.STRING;
+    }
+
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = { " " })
     void shouldThrowExceptionWhenNameIsNullOrBlank(String name) {
         assertThrows(InvalidHeaderException.class, () -> {
-            new Header(name, HeaderType.STRING);
+            new Header(name, type);
         });
     }
 
-    @Test
-    void shouldThrowExceptionWhenTypeIsNull() {
-        HeaderType type = null;
+    @ParameterizedTest
+    @NullSource
+    void shouldThrowExceptionWhenTypeIsNull(HeaderType type) {
+        String name = "header";
 
         assertThrows(InvalidHeaderException.class, () -> {
-            new Header("header", type);
+            new Header(name, type);
         });
     }
 
     @Test
     void shouldReturnTrueWhenComparingHeadersWithSameName() {
-        Header header1 = new Header("header", HeaderType.STRING);
-        Header header2 = new Header("header", HeaderType.STRING);
+        String name = "header";
+        Header header1 = new Header(name, type);
+        Header header2 = new Header(name, type);
 
         assertEquals(header1, header2);
     }
 
     @Test
     void shouldReturnFalseWhenComparingHeadersWithDifferentName() {
-        Header header1 = new Header("header1", HeaderType.STRING);
-        Header header2 = new Header("header2", HeaderType.STRING);
+        String name1 = "header1";
+        String name2 = "header2";
+        Header header1 = new Header(name1, type);
+        Header header2 = new Header(name2, type);
 
         assertNotEquals(header1, header2);
     }

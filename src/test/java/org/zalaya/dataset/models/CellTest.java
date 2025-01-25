f09be8945b910f93ca2afbb7c958a8f2b6edfd7f
Zalaya <enol.corzal@gmail.com>
2025-01-25 17:18:40 +0100

    feat: add tests for the cell model

--- src/test/java/org/zalaya/dataset/models/CellTest.java
+++ src/test/java/org/zalaya/dataset/models/CellTest.java
@@ -1,5 +1,40 @@
 package org.zalaya.dataset.models;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class CellTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldNotThrowExceptionWhenValueIsNullOrEmpty(String value) {
+        assertDoesNotThrow(() -> new Cell(value));
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenValueIsBlank() {
+        String value = " ";
+
+        assertDoesNotThrow(() -> new Cell(value));
+    }
+
+    @Test
+    void shouldReturnTrueWhenComparingCellsWithSameValues() {
+        Cell cell1 = new Cell(1);
+        Cell cell2 = new Cell(1);
+
+        assertEquals(cell1, cell2);
+    }
+
+    @Test
+    void shouldReturnFalseWhenComparingCellsWithDifferentValues() {
+        Cell cell1 = new Cell(1);
+        Cell cell2 = new Cell(2);
+
+        assertNotEquals(cell1, cell2);
+    }
 
 }

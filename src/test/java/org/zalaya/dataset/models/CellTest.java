c37f339175f3058ae74325f1e0369adaad4e90a9
Zalaya <enol.corzal@gmail.com>
2025-01-26 11:08:54 +0100

    test: add parameterized tests for cell instantiation and comparison methods

--- src/test/java/org/zalaya/dataset/models/CellTest.java
+++ src/test/java/org/zalaya/dataset/models/CellTest.java
@@ -1,5 +1,36 @@
 package org.zalaya.dataset.models;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class CellTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldInstantiateWithNullOrEmptyValue(String value) {
+        assertDoesNotThrow(() -> new Cell(value));
+    }
+
+    @Test
+    void shouldReturnTrueWhenComparingTwoCellsWithSameValue() {
+        String value = "value";
+        Cell cell1 = new Cell(value);
+        Cell cell2 = new Cell(value);
+
+        assertEquals(cell1, cell2);
+    }
+
+    @Test
+    void shouldReturnFalseWhenComparingTwoCellsWithDifferentValue() {
+        String value1 = "value1";
+        String value2 = "value2";
+        Cell cell1 = new Cell(value1);
+        Cell cell2 = new Cell(value2);
+
+        assertNotEquals(cell1, cell2);
+    }
 
 }

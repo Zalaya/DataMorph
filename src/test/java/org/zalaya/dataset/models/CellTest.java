9bf1033412d3bd4700ea1f4523a1c701dae279d0
Zalaya <enol.corzal@gmail.com>
2025-01-26 11:50:58 +0100

    test: add parameterized tests for dataset and cell instantiation validation

--- src/test/java/org/zalaya/dataset/models/CellTest.java
+++ src/test/java/org/zalaya/dataset/models/CellTest.java
@@ -1,6 +1,5 @@
 package org.zalaya.dataset.models;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -10,27 +9,8 @@ class CellTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    void shouldInstantiateWithNullOrEmptyValue(String value) {
+    void shouldNotThrowExceptionForNullOrEmptyValue(String value) {
         assertDoesNotThrow(() -> new Cell(value));
-    }
-
-    @Test
-    void shouldReturnTrueWhenComparingTwoCellsWithSameValue() {
-        String value = "value";
-        Cell cell1 = new Cell(value);
-        Cell cell2 = new Cell(value);
-
-        assertEquals(cell1, cell2);
-    }
-
-    @Test
-    void shouldReturnFalseWhenComparingTwoCellsWithDifferentValue() {
-        String value1 = "value1";
-        String value2 = "value2";
-        Cell cell1 = new Cell(value1);
-        Cell cell2 = new Cell(value2);
-
-        assertNotEquals(cell1, cell2);
     }
 
 }
--- src/test/java/org/zalaya/dataset/models/DatasetTest.java
+++ src/test/java/org/zalaya/dataset/models/DatasetTest.java
@@ -1,5 +1,41 @@
 package org.zalaya.dataset.models;
 
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.zalaya.dataset.enumerators.HeaderType;
+import org.zalaya.dataset.exceptions.InvalidDatasetException;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class DatasetTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    void shouldThrowExceptionForNullOrBlankName(String name) {
+        Set<Header> headers = Set.of(new Header("header", HeaderType.STRING));
+        List<Row> rows = List.of();
+
+        assertThrows(InvalidDatasetException.class, () -> {
+            new Dataset(name, headers, rows);
+        });
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldThrowExceptionForNullOrEmptyHeaders(Set<Header> headers) {
+        String name = "dataset";
+        List<Row> rows = List.of();
+
+        assertThrows(InvalidDatasetException.class, () -> {
+            new Dataset(name, headers, rows);
+        });
+    }
 
 }

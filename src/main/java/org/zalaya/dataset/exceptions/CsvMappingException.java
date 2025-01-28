6ec90f3a1eabafe0a08cb8138077c802cb7de595
Zalaya <enol.corzal@gmail.com>
2025-01-28 18:58:50 +0100

    feat: implement csv mapper class for csv file mapping and add csv mapping exception for error handling

---
+++ src/main/java/org/zalaya/dataset/exceptions/CsvMappingException.java
@@ -0,0 +1,9 @@
+package org.zalaya.dataset.exceptions;
+
+public class CsvMappingException extends RuntimeException {
+
+    public CsvMappingException(String message) {
+        super(message);
+    }
+
+}

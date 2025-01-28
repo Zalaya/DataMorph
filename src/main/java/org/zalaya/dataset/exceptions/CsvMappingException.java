49e853f2c7d499ccd7e7c8a00b99690904c169b3
Zalaya <enol.corzal@gmail.com>
2025-01-28 19:03:41 +0100

    chore: remove dataset header from readme file

--- src/main/java/org/zalaya/dataset/exceptions/CsvMappingException.java
+++
@@ -1,9 +0,0 @@
-package org.zalaya.dataset.exceptions;
-
-public class CsvMappingException extends RuntimeException {
-
-    public CsvMappingException(String message) {
-        super(message);
-    }
-
-}

bb844a7d9cabc2dbb9edc5a77962a105379e1e0f
Zalaya <enol.corzal@gmail.com>
2025-02-01 11:00:40 +0100

    feat: implement validation in header class and simplify code structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,13 @@ repositories {
     mavenCentral()
 }
 
+val lombokVersion = "1.18.36"
 val junitVersion = "5.10.0"
 val mockitoVersion = "5.15.2"
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
--- src/main/java/org/zalaya/datamorph/dataset/models/Header.java
+++ src/main/java/org/zalaya/datamorph/dataset/models/Header.java
@@ -1,44 +1,27 @@
 package org.zalaya.datamorph.dataset.models;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import org.zalaya.datamorph.dataset.enumerators.HeaderType;
+import org.zalaya.datamorph.dataset.exceptions.validation.HeaderValidationException;
 
-import java.util.Objects;
-
+@Getter
+@Builder
+@EqualsAndHashCode(exclude = "type")
 public class Header {
 
     private final String name;
     private final HeaderType type;
 
-    // TODO: A header can be created without a type.
-    // TODO: A header can't be created without a name.
-
-    // TODO: A header must have a valid name.
-
     public Header(String name, HeaderType type) {
-        this.name = name;
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public HeaderType getType() {
-        return type;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Header header)) {
-            return false;
+        if (name == null || name.trim().isBlank() || type == null) {
+            throw new HeaderValidationException("Header name and type must not be null or empty");
         }
 
-        return Objects.equals(name, header.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(name);
+        this.name = name;
+        this.type = type;
     }
 
 }

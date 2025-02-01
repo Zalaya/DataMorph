cc56106e9b8aa49a469f842d2b9d567ade7f9a9f
Zalaya <enol.corzal@gmail.com>
2025-02-01 13:30:02 +0100

    feat: add mock utilities class for mocking header type in tests

---
+++ src/test/java/org/zalaya/datamorph/dataset/utilities/MockUtilities.java
@@ -0,0 +1,17 @@
+package org.zalaya.datamorph.dataset.utilities;
+
+import org.zalaya.datamorph.dataset.enumerators.HeaderType;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class MockUtilities {
+
+    public static HeaderType mockHeaderType(Class<?> type) {
+        HeaderType mockHeaderType = mock(HeaderType.class);
+        doReturn(type).when(mockHeaderType).type();
+
+        return mockHeaderType;
+    }
+
+}
